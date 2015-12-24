b3d17ee1a3cac22201049c8f3b29678cdac44337
David Bebb <davidbebb@gmail.com>
2015-12-24 19:53:07 +0000

    added shows user name of logged in user on nav bar and refactored nav bar if-else

--- spec/feature/nav_bar_spec.rb
+++ spec/feature/nav_bar_spec.rb
@@ -5,4 +5,20 @@ feature 'nav bar' do
     visit '/'
     expect(page).to have_css('div#nav_bar')
   end
+
+  context 'User is logged in' do
+
+    before(:each) do
+        @user = FactoryGirl.create(:user)
+        visit '/users/sign_in'
+        fill_in 'Email', with: @user.email
+        fill_in 'Password', with: @user.password
+        click_button 'Log in'
+    end
+
+    scenario 'nav bar should display name of logged in user' do
+      expect(page).to have_content(@user.email)
+    end
+
+  end
 end
