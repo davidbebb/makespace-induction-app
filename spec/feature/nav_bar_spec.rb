8f83186906fd7277f25147d8cb621d827b0c35db
David Bebb <davidbebb@gmail.com>
2015-12-24 19:03:48 +0000

    minor update to nav_bar_spec

--- spec/feature/nav_bar_spec.rb
+++ spec/feature/nav_bar_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
 feature 'nav bar' do
-  context 'user not logged in' do
-    scenario 'should display a nav bar' do
-      visit '/'
-      expect(page).to have_css('div#nav_bar')
-    end
+  scenario 'should display a nav bar' do
+    visit '/'
+    expect(page).to have_css('div#nav_bar')
   end
 end
